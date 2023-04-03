1bd58aecdd1542f1f13ce2953ecb415f15b5674a
AJEDDIGElias <eajeddig@gmail.com>
2023-04-03 09:59:00 +0200

    Creating website router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Layout from './components/layout/Layout.js';
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage.js';
+import About from './pages/About.js';
+import Contact from './pages/Contact.js';
+import Policy from './pages/Policy.js';
+import PageNotFound from './pages/PageNotFound.js';
 
 function App() {
   return (
     <>
-    <Layout>
-      <h1>ECommerce App</h1>
-    </Layout>
+    
+    <Routes>
+      <Route path='/' element={<HomePage></HomePage>} ></Route>
+      <Route path='/about' element={<About></About>} ></Route>
+      <Route path='/contact' element={<Contact></Contact>} ></Route>
+      <Route path='/policy' element={<Policy></Policy>} ></Route>
+      <Route path='*' element={<PageNotFound></PageNotFound>} ></Route>
+    </Routes>
+
     </>
   );
 }
